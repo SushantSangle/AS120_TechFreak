2dfa5d14a28cab70a9331c6423217d19c2125820
MansiNangare <mansinangare@gmail.com>
2020-08-01 15:19:31 +0530

    chore: remove redundant code
    Signed-off-by: = <=>

--- SafeTrigger/app/src/main/java/com/Techfreaks/SafeTrigger/TriggerReceiver.kt
+++ SafeTrigger/app/src/main/java/com/Techfreaks/SafeTrigger/TriggerReceiver.kt
@@ -1,23 +1,11 @@
 package com.Techfreaks.SafeTrigger
 
-import android.Manifest
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Build
-import android.os.Handler
-import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import com.Techfreaks.Services.messageHelper
 import com.Techfreaks.utils.isValidTrigger
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 
 /*
 * Do not create multiple objects of this class with same value of n, may create a problem :[*/
@@ -30,29 +18,13 @@ class TriggerReceiver(private val nTapCount: Int, private val operation: (Contex
             Intent.ACTION_SCREEN_OFF -> {
                 if (isValidTrigger(context, nTapCount)) {
                     operation(context)
-                    if(nTapCount == 3) {
-                        Handler().post {
-                            val SOS_3_launch = Intent(context,com.Techfreaks.SafeTrigger.SOS_placeholder::class.java)
-                            SOS_3_launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                            context.startActivity(SOS_3_launch)
-                        }
-                    }
                 }
             }
             Intent.ACTION_SCREEN_ON -> {
                 if(isValidTrigger(context,nTapCount)) {
                     operation(context)
-                    if(nTapCount == 3) {
-                        Handler().post {
-                            val SOS_3_launch = Intent(context,com.Techfreaks.SafeTrigger.SOS_placeholder::class.java);
-                            SOS_3_launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                            context.startActivity(SOS_3_launch)
-                        }
-                    }
                 }
             }
         }
     }
-
-
 }
